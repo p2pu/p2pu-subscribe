faa10995561ba9a1efad89302a806fb12bf24348
Dirk Uys <dirkcuys@gmail.com>
2015-06-12 18:02:15 -0500

    Use default django email handling to send emails

--- signup/emails.py
+++ signup/emails.py
@@ -1,8 +1,16 @@
 from django.conf import settings
 from django.template.loader import render_to_string
+from django.core.email import send_mail
 
 def send_welcome_emails(emails):
     subject = render_to_string('signup/emails/signup-confirmation-subject.txt', {}).strip()
     text_body = render_to_string('signup/emails/signup-confirmation.txt', {}).strip()
     html_body = render_to_string('signup/emails/signup-confirmation.html', {}).strip()
-    mailgun.api.send_mass_email(emails, settings.DEFAULT_FROM_EMAIL, subject, text_body, html_body, tags=['signup'])
+    send_mail(
+        subject,
+        text_body,
+        settings.DEFAULT_FROM_EMAIL,
+        emails,
+        fail_silently=False,
+        html_message=html_body
+    )
