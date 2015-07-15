6995028e2196cc4c1a15fc7bc3533509250903ff
Dirk Uys <dirkcuys@gmail.com>
2015-07-15 17:30:42 -0500

    Move periodic task to tasks module since celery docs seems unclear on how to handle non standard modules for defining tasks

--- signup/tasks.py
+++ signup/tasks.py
@@ -6,3 +6,21 @@ from signup import emails
 @shared_task
 def send_welcome_email(signup):
     emails.send_welcome_email(signup)
+
+
+@shared_task
+def call_webhook(signup):
+    pass
+
+
+@shared_task
+def send_weekly_digest():
+    from signup.models import get_previous_week_signups
+    from signup.db import SignupScope
+    for scope in SignupScope.objects.all():
+        signups = get_previous_week_signups(scope.scope_name)
+        # send email to admin user
+        # TODO store per scope contact
+        # TODO give scope option for weekly digest
+        if len(signups) > 0:
+            emails.send_weekly_signup_digest(scope.scope_name, signups)
